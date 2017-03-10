0fd8ed0f6621b40237336c90a980142b29d0fb3e
Abhishek Deb <abhishekdeb91@gmail.com>
2017-03-10 06:14:32 +0530

    init

---
+++ index.js
@@ -0,0 +1,114 @@
+const fs = require('fs');
+
+
+//Init : Reads file and finds predicate. returns null if not found;
+var stringJect=function (filePath,predicate) {
+	this.filePath=filePath;
+	this.file=fs.readFileSync(filePath).toString().split('\n');
+	this.text='';
+	this.found=-1;
+
+
+	//SEARCH
+	for(var i=0;i<this.file.length;i++){
+		if(this.file[i].search(predicate)>=0){
+			this.found=i;
+			break;
+		}
+	}
+
+	if(this.found<0){
+		console.log('Predicate NOT found!. File untouched.');
+		return null;
+	}
+
+	return this;
+};
+
+
+stringJect.prototype.before = function(str){
+	this.file.splice((this.found), 0, str);
+	this.found++;
+	return this;
+};
+
+stringJect.prototype.after = function(str){
+	this.file.splice((this.found+1), 0, str);
+	return this;
+};
+
+stringJect.prototype.replace = function(str,lines){
+	if(lines===undefined || lines===null){
+		lines=1;
+	}
+
+	this.file.splice((this.found), lines, str);
+	return this;
+};
+
+stringJect.prototype.delete = function(lines){
+	if(lines===undefined || lines===null){
+		lines=1;
+	}
+	this.file.splice(this.found, lines);
+
+	return this;
+};
+
+
+//Deletes lines until predicate matches ( inclusive). If not found, deletes only 1 line
+stringJect.prototype.deleteUntill = function(deletePredicate){
+	var deleteUntilLine=1;
+
+	//SEARCH
+	for(var i=0;i<this.file.length;i++){
+		if(this.file[i].search(deletePredicate)>=0){
+			deleteUntilLine=i;
+			break;
+		}
+	}
+
+	//console.log(deleteUntilLine);
+
+	this.file.splice(this.found, (deleteUntilLine-this.found)+1);
+
+	return this;
+};
+
+
+
+stringJect.prototype.find = function(){
+	return this.found>0;
+};
+
+
+stringJect.prototype.save = function(cb){
+	this.text = this.file.join("\n");
+
+	//WRITE
+	fs.writeFile(this.filePath, this.text, function (err) {
+		cb(err);
+	}); 
+};
+
+stringJect.prototype.saveSync = function(){
+	this.text = this.file.join("\n");
+
+	//WRITE
+	fs.writeFileSync(this.filePath, this.text);
+
+	return this;
+};
+
+
+
+
+exports=module.exports=stringJect;
+
+
+/*
+//Test
+var s=new stringJect('./sample.txt','This is 8th').delete().after('hehehe').saveSync();
+console.log('DONE ',s);
+
+*/
---
+++ test.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const stringJect=require('./index.js');
+
+var predicate='How are y';
+var file='./sample.txt';
+
+var passed=0,failed=0;
+
+//INSERT before
+var str='New before Line';
+
+test('insert before',str,()=>{
+	new stringJect('./sample.txt',predicate).before(str).saveSync(); //Actual Write
+});
+
+
+str='After Line';
+
+test('insert after',str,()=>{
+	new stringJect('./sample.txt',predicate).after(str).saveSync(); 
+});
+
+
+
+str='Replaced Line';
+
+test('Replace',str,()=>{
+	new stringJect('./sample.txt',predicate).replace(str).saveSync(); 
+});
+
+
+new stringJect('./sample.txt',str).replace('How are you?').saveSync(); //Revert for further testing
+
+
+
+str='Did you';
+
+testNot('Delete',str,()=>{
+	new stringJect('./sample.txt',str).delete().saveSync(); 
+});
+
+
+
+
+
+
+
+console.log('Tests  \u2713 Passed :  '+passed+'.  \nTests \u2613 Failed : '+failed);
+
+
+function test(title,expected,run){
+	console.log('\nTesting "'+title+'" ...');
+	run();
+	var s=new stringJect(file,expected).find(); //Actual Write
+	var p='';
+	if(s){
+		passed++;
+		p=' \u2713 PASSED.';
+	}else{
+		failed++;
+		p=' \u2613 FAIL!';
+	}
+
+	
+	console.log('Test "'+title+'"  '+p);
+	console.log('===========================\n\n');
+}
+
+
+function testNot(title,expected,run){
+	console.log('\nTesting "'+title+'" ...');
+	run();
+	var s=new stringJect(file,expected).find(); //Actual Write
+	var p='';
+	if(!s){
+		passed++;
+		p=' \u2713 PASSED.';
+	}else{
+		failed++;
+		p=' \u2613 FAIL!';
+	}
+
+	
+	console.log('Test "'+title+'"  '+p);
+	console.log('===========================\n\n');
+}
+
+
+
